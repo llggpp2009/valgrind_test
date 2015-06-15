0b4f08319888217bebcff7e821c43359621939c0
liuguoping <liuguoping@jd.com>
2015-06-15 14:59:29 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#LOG_FILE=%p_log.memcheck
+#valgrind --tool=memcheck --leak-check=full --log-file=${LOG_FILE} --show-reachable=yes --track-origins=yes ${TARGET}   
 TARGET=./test
-LOG_FILE=%p_log.memcheck
 XML_FILE=%p_log.memcheck
 
 make
-valgrind --tool=memcheck --leak-check=full --log-file=${LOG_FILE} --xml=yes --xml-file=${XML_FILE} --show-reachable=yes --track-origins=yes ${TARGET}   
+valgrind --tool=memcheck --xml=yes --xml-file=${XML_FILE} --show-reachable=yes --track-origins=yes ${TARGET}   
+#valgrind --xml=yes --xml-file=${XML_FILE} --show-reachable=yes --track-origins=yes ${TARGET}   
+#valgrind --xml=yes --xml-file=${XML_FILE} ${TARGET}   
 make clean
