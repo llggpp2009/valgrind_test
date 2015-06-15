d6b8f1d0f876f2c6f5b6c58389d4dce26b4fe784
llggpp2009 <llggpp2009@163.com>
2015-06-15 11:30:21 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TARGET=./test
-LOG_FILE=%p_log.memcheck
-XML_FILE=%p_log.memcheck
-
-make
-valgrind --tool=memcheck --leak-check=full --log-file=${LOG_FILE} --xml=yes --xml-file=${XML_FILE} --show-reachable=yes --track-origins=yes ${TARGET}   
-make clean
